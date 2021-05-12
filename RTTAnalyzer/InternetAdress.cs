88dde1b617cd2c69b4ec8032b905ea1fdf8fa146
emyashev <emyashev2011@gmail.com>
2021-05-12 13:58:55 +0300

    add intenet ip config and script for interaction with it

---
+++ RTTAnalyzer/InternetAdress.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTTAnalyser
+{
+    class InternetAdress
+    {
+        private Dictionary<string, string> dnsIpPair = new Dictionary<string, string>();
+
+        public InternetAdress()
+        {
+            dnsIpPair = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("IpConfig.json"));
+        }
+
+        public string GetIp(string dns)
+        {
+            return dnsIpPair[dns];
+        }
+
+        public List<string> GetAllDns()
+        {
+            return dnsIpPair.Keys.ToList();
+        }
+    }
+}
